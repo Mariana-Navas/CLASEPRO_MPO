fd253663ba76f80d99856308c43ce79da19873d6
Mariana-Navas <navasparejo@gmail.com>
2025-10-27 19:24:58 +0100

    update

--- TEMA1/ProyectoInicial/src/Main.java
+++ TEMA1/ProyectoInicial/src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+
+    //Esto es un comentario simple
+
+    /*
+    esto es un comentario
+    en varias líneas
+    de código
+     */
+
+    //TODO Esto es un comentario resaltado con "TODO"
+
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en Java :)");
+        System.out.println("Esta es mi segunda línea de Java ever :O");
+        // Si quiero hacer un salto de línea puedo usar \n dentro de la frase entrecomillada
+    }
+
+
+    public void nombreMetodo (){
+
+
+    }
+
 }
